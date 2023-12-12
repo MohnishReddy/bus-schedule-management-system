cfbf817cd7cb3330993b971310e518bde0ad274e
Mohnish <mscreddy09@gmail.com>
2023-12-13 00:08:45 +0530

    Init project with spring boot java17 maven

---
+++ src/test/java/com/example/bsms/BsmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
